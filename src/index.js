ba2357fd781524e15367f7ad856a2b4a8d163d2c
Alena Astravukh <helen151204@gmail.com>
2020-10-19 14:54:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function toReadable (number) {
-  
+
+module.exports = function toReadable (n) {
+    if (n === 0) {
+        return 'zero';
+    }
+
+    let words = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    let dozenWords = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let extraWords = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    let nString = n.toString().split('');
+
+    if (n < 21) {
+        return words[n];
+    }
+
+    if(nString.length === 2) {
+        if(nString[1] !== '0') {
+            return `${dozenWords[nString[0]]} ${words[nString[1]]}`;
+        } else if(nString[1] === '0') {
+            return `${dozenWords[nString[0]]}`;
+        }
+    }
+
+    if(nString.length === 3) {
+        if(nString[1] > '1' && nString[2] !== '0') {
+            return `${words[nString[0]]} hundred ${dozenWords[nString[1]]} ${words[nString[2]]}`;
+        } else if(nString[1] === '0' && nString[2] !== '0') {
+            return `${words[nString[0]]} hundred ${words[nString[2]]}`;
+        } else if(nString[1] > '1' && nString[2] === '0') {
+            return `${words[nString[0]]} hundred ${dozenWords[nString[1]]}`;
+        } else if(nString[1] === '0' && nString[2] === '0') {
+            return `${words[nString[0]]} hundred`;
+        } else if(nString[1] === '1') {
+            return `${words[nString[0]]} hundred ${extraWords[nString[2]]}`;
+        }
+    }
+
 }
